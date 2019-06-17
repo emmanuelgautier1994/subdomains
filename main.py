0925b35e4e2ea145e052f072001f5aa519d0933a
emmanuel.gautier <emmanuel.gre@gmail.com>
2019-06-17 17:16:44 +0200

    added comments

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
-# f = open("gouv.fr.txt", "r").read().splitlines()
 import csv
 
-domain_tree = {}
+domain_tree = {} # this dictionary will bear the tree structure 
 
-with open('gouv.fr.csv') as csv_file:
+with open('gouv.fr.csv') as csv_file: # reading CSV and inserting into dictionary
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
-        if line_count > 0:
+        if line_count > 0: # leaving CSV header out
             x = row[0]
-            url = x.split('.')[::-1]
+            url = x.split('.')[::-1] # e.g. "www.gouv.fr" => ["fr", "gouv", "www"]
             dict_pointer = domain_tree
 
-            while(url):
+            while(url): # inserting the URL's domain levels as nested dicts within domain_tree
             	if not(url[0] in dict_pointer.keys()):
             		dict_pointer[url[0]] = {}
-            	dict_pointer = dict_pointer[url[0]]
+            	dict_pointer = dict_pointer[url[0]] # moving the pointer one level deeper
             	url.pop(0)
 
-            dict_pointer["__node__"] = x
+            dict_pointer["__node__"] = x # once at URL's tip, writing the full URL at an arbitrary "__node__" key
 
         line_count += 1
 
-def recDrawTree(dict, original_depth):
+def recDrawTree(dict, original_depth): # recursively prints tree represented by dict, with memory of depth level
 	keys = list(dict)
 	keys.sort()
 
